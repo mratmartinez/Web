de20e7a212eb07c3eefb899b1c25bfc9b62103ff
Juan Martínez <mratmartinez@anche.no>
2019-05-29 00:32:24 -0300

    Now we can render the posts list on the homepage

--- Juancito/blog/views.py
+++ Juancito/blog/views.py
@@ -3,7 +3,7 @@ from blog.models import Blog
 from django.views.generic import ListView
 
 class Blogposts(ListView):
-    queryset = Blog.objects.order_by('-publication_date')
+    queryset = Blog.objects.order_by('-post_date')
     template_name = "index.html"
     paginate_by = 5
     context_object_name = 'blog_list'
