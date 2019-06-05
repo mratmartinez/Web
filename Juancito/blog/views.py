c90173b1c21ae2d5821ae61228dbdf689cfb249a
Juan Martínez <mratmartinez@anche.no>
2019-06-05 19:24:28 -0300

    Now it can load a post!
    After some time fighting with DetailView, and realizing that it was much
    simpler than I expected.

--- Juancito/blog/views.py
+++ Juancito/blog/views.py
@@ -1,9 +1,14 @@
 from blog.models import Blog
 
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 class Blogposts(ListView):
     queryset = Blog.objects.order_by('-post_date')
-    template_name = "index.html"
+    template_name = 'index.html'
     paginate_by = 5
     context_object_name = 'blog_list'
+
+class Post(DetailView):
+    template_name = 'post.html'
+    model = Blog
+    context_object_name = 'post'
