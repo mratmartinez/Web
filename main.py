b6861c2be5a8e144c7436cfb983018a671ada6e5
Juan Martínez <mratmartinez@anche.no>
2019-09-18 23:19:36 -0300

    Now my old web is almost working

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+TEMPLATES_FOLDER = 'templates'
 
 @app.route('/')
 def index():
-    return 'This is the test'
+    return render_template("index.html")
 
 @app.route('/about')
 def about():
