4f6bae849cca7968b35793e86edf4b03d5d90d82
Juan Martínez <mratmartinez@anche.no>
2019-09-19 17:29:18 -0300

    My web can now display Markdown!
    TODO:
    -Fix the CSS
    -Add Metadata
    -Add Footnotes
    -Add abbr support
    -Links should have a different color.

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+import glob
+import os
+from markdown import markdown
 from flask import Flask, render_template
+
+DEBUG = True
+POSTS_FOLDER = "posts"
+SITE_NAME = 'La web de Juancito'
+
 app = Flask(__name__)
 
-TEMPLATES_FOLDER = 'templates'
+def get_posts():
+    filelist = glob.glob(os.path.join(POSTS_FOLDER, '*.md'))
+    posts = list()
+    for i in filelist:
+        with open(i, 'r') as file:
+            posts.append(markdown(file.read(), extensions=['fenced_code']))
+    return posts
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    posts = get_posts()
+    return render_template("index.html", blog_list=posts)
 
 @app.route('/about')
 def about():
