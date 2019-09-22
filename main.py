4f69cf69ad49ae46dfb13d495eab5d2f3a4a63ff
Juan Martínez <mratmartinez@anche.no>
2019-09-22 20:23:18 -0300

    Now it saves to a cache, but it's still not finished
    Markdowns should only be checked when they are modified.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-import glob
 import os
+import glob
+import json
+import hashlib
 import markdown
+from slugify import slugify
 from flask import Flask, render_template
 
-DEBUG = True
-POSTS_FOLDER = "posts"
 SITE_NAME = 'La web de Juancito'
+DEBUG = True
+POSTS_FOLDER = 'posts'
+CACHE_FOLDER = 'cache'
+# For some reason, Markdown module's "meta" extension creates a list for
+# every value so we need to call them with index=0
+LIST_TAGS = ['title', 'date', 'summary']
+# Now that I added this declaration, it's a great moment to explain that the
+# date tag *must* be formated following the ISO 8601
 
 app = Flask(__name__)
 md = markdown.Markdown(extensions=['fenced_code', 'meta'])
@@ -15,11 +24,27 @@ def get_posts():
     posts = list()
     for i in filelist:
         with open(i, 'r') as file:
-            html = md.convert(file.read())
+            data = file.read()
+            checksum = hashlib.md5(bytes(data.encode("utf-8"))).hexdigest()
+            html = md.convert(data)
             post = md.Meta
+            post['checksum'] = checksum
             post['content'] = html
+            # Those values are outside of a list, unlike the ones generated
+            # automatically. So we'll iterate over the rest of the tags in
+            # order to make some consistence adjustments.
+            for tag in LIST_TAGS:
+                post[tag] = post[tag][0]
+            post_to_cache(post)
             posts.append(post)
     return posts
+
+def post_to_cache(post_dict):
+    meta = json.dumps(post_dict)
+    slug = post_dict['date'] + '-' + slugify(post_dict['title'])
+    filename = os.path.join(CACHE_FOLDER, slug) + '.json'
+    with open(filename, 'w') as cache:
+        cache.write(meta)
 
 @app.route('/')
 def index():
