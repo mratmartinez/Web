8c55df0eb19fabcbf66a356ba95fe195c3aa22c0
Juan Gabriel Martinez <martinez@juanci.to>
2019-09-25 22:48:18 -0300

    Now we have a proper caching system!
    And it was the first time in my life that I found a use
    for lambda in Python.

--- main.py
+++ main.py
@@ -10,45 +10,56 @@ SITE_NAME = 'La web de Juancito'
 DEBUG = True
 POSTS_FOLDER = 'posts'
 CACHE_FOLDER = 'cache'
-# For some reason, Markdown module's "meta" extension creates a list for
-# every value so we need to call them with index=0
-LIST_TAGS = ['title', 'date', 'summary']
-# Now that I added this declaration, it's a great moment to explain that the
-# date tag *must* be formated following the ISO 8601
 
 app = Flask(__name__)
 md = markdown.Markdown(extensions=['fenced_code', 'meta'])
 
-def get_posts():
+def load_cache():
+    filelist = os.listdir(CACHE_FOLDER)
+    # This one-liner does the same but I think that explicitly closing files is
+    # a better practice in those cases.
+    # [json.loads(open(os.path.join(CACHE_FOLDER, i), 'r').read()) for i in filelist]
+    post_list = list()
+    for i in filelist:
+        filename = os.path.join(CACHE_FOLDER, i)
+        with open(filename, 'r') as file:
+            metadata = json.loads(file.read())
+            post_list.append(metadata)
+    return post_list
+
+def get_posts(post_list):
     filelist = glob.glob(os.path.join(POSTS_FOLDER, '*.md'))
-    posts = list()
+    filename_filter = lambda item: item['filename'] == base_file
     for i in filelist:
         with open(i, 'r') as file:
             data = file.read()
-            checksum = hashlib.md5(bytes(data.encode("utf-8"))).hexdigest()
-            html = md.convert(data)
-            post = md.Meta
-            post['checksum'] = checksum
-            post['content'] = html
-            # Those values are outside of a list, unlike the ones generated
-            # automatically. So we'll iterate over the rest of the tags in
-            # order to make some consistence adjustments.
-            for tag in LIST_TAGS:
-                post[tag] = post[tag][0]
-            post_to_cache(post)
-            posts.append(post)
-    return posts
+            checksum = hashlib.md5(bytes(data.encode('utf-8'))).hexdigest()
+            if checksum not in [j['checksum'] for j in post_list]:
+                html = md.convert(data)
+                base_file = os.path.basename(i)
+                try:
+                    post_list.remove(next(filter(filename_filter, post_list)))
+                except StopIteration:
+                    pass # This means that this post isn't an update
+                post = md.Meta
+                post['filename'] = base_file
+                post['slug'] = slugify(post['title'][0])
+                post['checksum'] = checksum
+                post['content'] = html
+                save_to_cache(post)
+                post_list.append(post)
+    return post_list
 
-def post_to_cache(post_dict):
+def save_to_cache(post_dict):
     meta = json.dumps(post_dict)
-    slug = post_dict['date'] + '-' + slugify(post_dict['title'])
-    filename = os.path.join(CACHE_FOLDER, slug) + '.json'
+    filename = os.path.join(CACHE_FOLDER, post_dict['filename'][0:-3]) + '.json'
     with open(filename, 'w') as cache:
         cache.write(meta)
 
 @app.route('/')
 def index():
-    posts = get_posts()
+    posts = load_cache()
+    posts = get_posts(posts)
     return render_template("index.html", blog_list=posts)
 
 @app.route('/about')
