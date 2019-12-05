80b404fec74892328fb3fd93d57725a82648ada1
Juan Martínez <mratmartinez@anche.no>
2019-12-05 12:25:05 -0300

    The code is now better commented.

--- juancito.py
+++ juancito.py
@@ -29,12 +29,14 @@ locale.setlocale(locale.LC_TIME, "es_AR")
 
 
 def slugify(text):
+    """Returns a slug string valid for URLs."""
     ascii_text = unidecode.unidecode(text)
     return re.sub(r'[-\s]+', '-',
                   (re.sub(r'[^\w\s-]', '', ascii_text).strip().lower()))
 
 
 def get_cache_files(folder):
+    """List the files in a folder. If the folder doesn't exists, it creates it."""
     try:
         files = os.listdir(folder)
     except FileNotFoundError:
@@ -44,12 +46,13 @@ def get_cache_files(folder):
 
 
 def load_cache():
+    """Read the posts in CACHE_FOLDER and returns a list with the results."""
     filelist = [os.path.join(CACHE_FOLDER, i) for i in get_cache_files(CACHE_FOLDER)]
     postlist = [json.loads(open(cache, 'r').read()) for cache in filelist]
     return postlist
 
-
 def get_file_data(filename):
+    """Read the contents of a post's file and return it along with its MD5 hash."""
     with open(filename, 'r') as opened:
         data = opened.read()
         checksum = hashlib.md5(bytes(data.encode('utf-8'))).hexdigest()
@@ -57,9 +60,10 @@ def get_file_data(filename):
 
 
 def format_date(datestring):
+    """Returns a properly formatted date according to the language used."""
     day = datetime.strptime(datestring, "%Y-%m-%d")
-    print(datestring)
     if locale.getlocale(locale.LC_TIME)[0] == "es_AR":
+        # In case we are displaying the web in spanish.
         formatted_day = day.strftime("%A %d de %B de %Y")
         capitalized_day = string.capwords(formatted_day, " de ")
     else:
@@ -68,6 +72,7 @@ def format_date(datestring):
 
 
 def update_md(basefile, data, checksum):
+    """Reads a Markdown file and returns a dict with metadata."""
     html = md.convert(data)
     post = md.Meta
     post['filename'] = basefile
@@ -78,7 +83,19 @@ def update_md(basefile, data, checksum):
     return post
 
 
+def save_to_cache(post_dict):
+    """Saves the recently updated Markdown in the cache as a JSON file."""
+    meta = json.dumps(post_dict)
+    filename = os.path.join(CACHE_FOLDER, post_dict['filename'][0:-3]) + '.json'
+    with open(filename, 'w') as cache:
+        cache.write(meta)
+
+
 def check_new_posts(postlist):
+    """
+    Checks if there are new posts or updates to the old ones.
+    In any case, it returns a new list with all the posts, updated or not.
+    """
     filelist = glob.glob(os.path.join(POSTS_FOLDER, '*.md'))
     filename_filter = lambda item: item['filename'] == basefile  # noqa
     for i in filelist:
@@ -96,6 +113,10 @@ def check_new_posts(postlist):
 
 
 def get_posts_list(update=False):
+    """
+    Loads only the cached posts, unless the 'update' flag is True.
+    In that case, it will check for new posts, even if no changes were made.
+    """
     posts = load_cache()
     if update:
         posts = check_new_posts(posts)
@@ -103,17 +124,11 @@ def get_posts_list(update=False):
 
 
 def get_post_from_slug(slug):
+    """Receives an slug string and returns the matching post."""
     slug_filter = lambda item: item['slug'] == slug  # noqa
     posts = get_posts_list()
     post = next(filter(slug_filter, posts))
     return post
-
-
-def save_to_cache(post_dict):
-    meta = json.dumps(post_dict)
-    filename = os.path.join(CACHE_FOLDER, post_dict['filename'][0:-3]) + '.json'
-    with open(filename, 'w') as cache:
-        cache.write(meta)
 
 
 @app.route('/')
