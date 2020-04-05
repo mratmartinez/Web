cba8ec49b27c32fd59833b5354e5cefe35ff1f21
Juan Gabriel Martinez <martinez@juanci.to>
2020-04-04 23:20:59 -0300

    Clean up removing commented code

--- juancito.py
+++ juancito.py
@@ -6,10 +6,10 @@ import json
 import string
 import locale
 import hashlib
-import markdown
 import configparser
 from datetime import datetime
 
+import markdown
 import unidecode
 from flask import Flask, render_template
 from flask_caching import Cache
@@ -60,39 +60,6 @@ def slugify(text):
                   (re.sub(r'[^\w\s-]', '', ascii_text).strip().lower()))
 
 
-# def get_cache_files(folder):
-#     """
-#     List the files in a folder. If the folder doesn't exists, it creates it.
-#     """
-#     try:
-#         files = os.listdir(folder)
-#     except FileNotFoundError:
-#         os.mkdir(folder)
-#         files = list()
-#     logger.debug(files)
-#     return files
-
-
-# def load_cache():
-#     """
-#     Read the posts in CACHE_FOLDER and returns a list with the results.
-#     """
-#     cachefiles = get_cache_files(CACHE_FOLDER)
-#     filelist = [os.path.join(CACHE_FOLDER, i) for i in cachefiles]
-#     postlist = [json.loads(open(cache, 'r').read()) for cache in filelist]
-#     return postlist
-
-
-# def get_file_data(filename):
-#     """
-#     Read the contents of a post's file and return it along with its MD5 hash.
-#     """
-#     with open(filename, 'r') as opened:
-#         data = opened.read()
-#         checksum = hashlib.md5(bytes(data.encode('utf-8'))).hexdigest()
-#     return data, checksum
-
-
 def format_date(datestring):
     """
     Returns a properly formatted date according to the language used.
@@ -108,21 +75,6 @@ def format_date(datestring):
     return capitalized_day
 
 
-# def update_md(basefile, data, checksum):
-#     """
-#     Reads a Markdown file and returns a dict with metadata.
-#     """
-#     html = md.convert(data)
-#     post = md.Meta
-#     post['filename'] = basefile
-#     post['slug'] = slugify(post['title'][0])
-#     post['checksum'] = checksum
-#     post['date'] = post['date'][0]
-#     post['form_date'] = format_date(post['date'])
-#     post['content'] = html
-#     return post
-
-
 def format_post(data):
     """
     Receives a post metadata and format it properly in order to use it.
@@ -134,38 +86,6 @@ def format_post(data):
     post['form_date'] = format_date(post['date'])
     post['content'] = html
     return post
-
-
-# def save_to_cache(post_dict):
-#     """
-#     Saves the recently updated Markdown in the cache as a JSON file.
-#     """
-#     meta = json.dumps(post_dict)
-#     postname = post_dict['filename'][0:-3]
-#     filename = os.path.join(CACHE_FOLDER, postname) + '.json'
-#     with open(filename, 'w') as cache:
-#         cache.write(meta)
-
-
-# def check_new_posts(postlist):
-#     """
-#     Checks if there are new posts or updates to the old ones.
-#     In any case, it returns a new list with all the posts, updated or not.
-#     """
-#     filelist = glob.glob(os.path.join(POSTS_FOLDER, '*.md'))
-#     def filename_filter(item): item['filename'] == basefile
-#     for i in filelist:
-#         data, checksum = get_file_data(i)
-#         if checksum not in [j['checksum'] for j in postlist]:
-#             basefile = os.path.basename(i)
-#             post = update_md(basefile, data, checksum)
-#             try:
-#                 postlist.remove(next(filter(filename_filter, postlist)))
-#             except StopIteration:
-#                 pass  # This means that this post isn't an update
-#             save_to_cache(post)
-#             postlist.append(post)
-#     return postlist
 
 
 def read_posts():
@@ -180,27 +100,6 @@ def read_posts():
             post = format_post(data)
             postlist.append(post)
     return postlist
-
-
-# def sort_posts_by_date(postlist):
-#     """
-#     This is a bugfix. Self-explained.
-#     """
-#     def post_filter(item): return datetime.strptime(item['date'], "%Y-%m-%d")
-#     postlist = sorted(postlist, key=post_filter)
-#     logger.debug([post['date'] for post in postlist])
-#     return postlist
-
-
-# def get_posts_list(update=True):
-#     """
-#     This is the old version of this function.
-#     """
-#     posts = load_cache()
-#     if update:
-#         posts = check_new_posts(posts)
-#     postlist = sort_posts_by_date(posts)
-#     return postlist
 
 
 @cache.memoize(timeout=50)
