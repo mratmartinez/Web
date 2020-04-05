ff7bf2c613495a3216ecd667d98199be33326a1f
Juan Gabriel Martínez <juan.martinez@super7ui.com>
2020-04-05 00:06:28 -0300

    Moved slugify to utils.py

--- utils.py
+++ utils.py
@@ -5,3 +5,12 @@ handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(funcName)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+def slugify(text):
+    """
+    Returns a slug string valid for URLs.
+    """
+    ascii_text = unidecode.unidecode(text)
+    return re.sub(r'[-\s]+', '-',
+                  (re.sub(r'[^\w\s-]', '', ascii_text).strip().lower()))
+
