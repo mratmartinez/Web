36975e1f9673b6253a9c1896a6ca6b61a7e43a3e
Juan Gabriel Martinez <martinez@juanci.to>
2020-08-29 07:10:57 -0300

    La verdad ni iddea en qué dejé esto pero lo dejo así

--- juancito.py
+++ juancito.py
@@ -43,7 +43,6 @@ STATIC_FOLDER = config['FILESYSTEM']['StaticFolder']
 ABOUT_FILE = config['FILESYSTEM']['AboutFile']
 
 
-<<<<<<< HEAD
 def slugify(text):
     """
     Returns a slug string valid for URLs.
@@ -144,86 +143,4 @@ def about():
         data = file.read()
         content = md.convert(data)
     return render_template("about.html", content=content)
-=======
-class Juancito:
-    def format_post(self, data):
-        """
-        Receives a post metadata and format it properly in order to use it.
-        """
-        html = md.convert(data)
-        post = md.Meta
-        post['slug'] = slugify(post['title'][0])
-        post['date'] = post['date'][0]
-        if post['language'][0] == "ES":
-            locale = "es_AR.UTF-8"
-        else:
-            locale = "en_GB.UTF-8"
-        date = datetime.strptime(post['date'], "%Y-%m-%d")
-        post['form_date'] = format_date(date=date,
-                                        format="full",
-                                        locale=locale)
-        post['content'] = html
-        return post
 
-
-    def read_posts(self):
-        """
-        Reads the .md files and converts it to Markdown.
-        """
-        postlist = list()
-        filelist = glob.glob(os.path.join(POSTS_FOLDER, '*.md'))
-        for i in filelist:
-            with open(i, 'r') as opened:
-                data = opened.read()
-                post = format_post(data)
-                postlist.append(post)
-        return postlist
-
-
-    @cache.memoize(timeout=50)
-    def get_post_from_slug(self, slug):
-        """
-        Receives an slug string and returns the matching post.
-        """
-        posts = get_postlist()
-        post = next(filter(lambda item: item['slug'] == slug, posts))
-        LOGGER.debug((type(post), id(post)))
-        return post
-
-
-    @cache.memoize(timeout=50)
-    def get_postlist(self):
-        """
-        Loads only the cached posts, unless the 'update' flag is True.
-        In that case, it will check for new posts, even if no changes were made.
-        Merged with sort_posts_by_date()
-        """
-        postlist = read_posts()
-        def post_filter(item): return datetime.strptime(item['date'], "%Y-%m-%d")
-        postlist = sorted(postlist, key=post_filter)
-        LOGGER.debug([post['date'] for post in postlist])
-        return postlist
-
-
-    @cache.cached(timeout=50)
-    @app.route('/')
-    def index(self):
-        posts = get_postlist()
-        return render_template("index.html", blog_list=reversed(posts))
-
-
-    @cache.cached(timeout=50)
-    @app.route('/post/<slug>')
-    def post(self, slug):
-        post = get_post_from_slug(slug)
-        return render_template("post.html", post=post)
-
-
-    @cache.cached(timeout=50)
-    @app.route('/about')
-    def about(self):
-        with open(ABOUT_FILE, 'r') as file:
-            data = file.read()
-            content = md.convert(data)
-        return render_template("about.html", content=content)
->>>>>>> prod
