75c879d4dd5eaaed9d6ef1d4f42e1b534fda1ce1
Juan Gabriel Martínez <martinez@juanci.to>
2022-06-11 00:47:58 +0200

    Resurrect the old design

--- yew/src/app.rs
+++ yew/src/app.rs
@@ -2,11 +2,22 @@ use yew::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let header: Html = html! {
+        <header>
+            <div class="nav center">
+                <div class="row">
+                    <div class="col"><h1 class="nav-logo"><a href="/">{"Juancito"}</a></h1></div>
+                    <div class="col"><a class="nav-item" href="/about">{"about"}</a></div>
+                    <div class="col"><a class="nav-item" href="https://www.github.com/mratmartinez">{"git"}</a></div>
+                </div>
+            </div>
+        </header>
+    };
+
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+            {header}
+            <p>{ "Test" }</p>
         </main>
     }
 }
