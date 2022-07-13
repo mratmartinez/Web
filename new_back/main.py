37e0502b7493a4358fd0b4fb905f30e3c6b8da14
Juan Gabriel Martínez <martinez@juanci.to>
2022-07-13 19:59:25 +0400

    Add filter for Markdown

--- new_back/main.py
+++ new_back/main.py
@@ -3,6 +3,8 @@ from worker import Worker
 def main():
     main_worker = Worker.get_worker_for_directory('.')
     print(main_worker.root_directory)
+    files = main_worker.scan_root_for_markdown()
+    print(list(files))
 
 if __name__ == '__main__':
     main()
--- new_back/worker.py
+++ new_back/worker.py
@@ -23,7 +23,7 @@ class Worker(object):
     @root_directory.setter
     def root_directory(self, directory: str):
         if not (os.path.isdir(directory)):
-                raise Exception(f'{directory} is not a valid directory')
+            raise Exception(f'{directory} is not a valid directory.')
         self._root_directory = directory
         return
 
@@ -33,3 +33,20 @@ class Worker(object):
         worker.root_directory = directory
         return worker
 
+    @staticmethod
+    def get_filename_filter(extension: str):
+        if not (extension.startswith('.') and len(extension)>1):
+            raise Exception(f'{extension} is not a valid extension suffix.')
+        lowercase_extension = extension.lower()
+        def filename_filter(filename: str):
+            lowercase_filename = filename.lower()
+            if (lowercase_filename.endswith(lowercase_extension) and not lowercase_filename.startswith('.')):
+                return True
+            return False
+        return filename_filter
+
+    def filter_root_for_markdown(self):
+        files = os.listdir(self.root_directory)
+        filename_filter = self.get_filename_filter('.md')
+        filtered_files = filter(filename_filter, files)
+        return filtered_files
